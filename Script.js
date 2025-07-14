3d2d3204693200d0a35386e8976cec1180a335d5
Ujjawal Thakur <ujjawalthakur896@gmail.com>
2025-07-14 14:32:42 +0530

    Add files via upload
    This is code for "Guess a number" Game.

---
+++ Script.js
@@ -0,0 +1,6 @@
+let gamenumber = 23;
+let num = prompt("Guess the number between 0 to 25");
+while(num != gamenumber){
+    num = prompt("Sorry you guess wrong, Guess again");
+}
+console.log("Right Guess");
